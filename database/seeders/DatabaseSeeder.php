828c17b81657d84992474c627a22b296d1c74821
tora180405 <toradigda@gmail.com>
2022-10-28 13:36:28 +0700

    commit knt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\jenis_kontak;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -17,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
 
         // $this->call([
-        //     UserSeeder::class,
+        //    UserSeeder::class,
         // ]);
 
         User::create([
@@ -26,6 +28,14 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('123456'),
             'role' => 'admin'
         ]);
+
+        jenis_kontak::created([
+            'whatsapp',
+            'line',
+            'facebook',
+            'instagram'
+        ]);
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
