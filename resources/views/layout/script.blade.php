eac9b777a51f80e12857bdd21ddda9ccee510371
tora180405 <toradigda@gmail.com>
2022-11-10 22:12:58 +0700

    commit ke 21

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -24,13 +24,13 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src='Template/vendor/jquery/jquery.min.js'></script>
-    <script src='Template/vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
+    <script src="Template/vendor/jquery/jquery.min.js"></script>
+    <script src="Template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src='Template/vendor/jquery-easing/jquery.easing.min.js'></script>
+    <script src="Template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src='Template/js/sb-admin-2.min.js'></script>
+    <script src="Template/js/sb-admin-2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
